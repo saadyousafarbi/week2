53ab8cd3b1b4783d4d77f244203f8731a2856960
Saad Yousaf <saad.yousaf@arbisoft.com>
2017-07-19 17:05:19 +0500

    Minor Changes, adding .html files to gitignore

--- tutorial/tutorial/spiders/quotes_spider_xpath.py
+++ tutorial/tutorial/spiders/quotes_spider_xpath.py
@@ -9,12 +9,15 @@ class quotes(scrapy.Spider):
     ]
 
     def parse(self, response):
+        count = 0
         for quote in response.xpath('//div[@class="quote"]'):
             yield {
-                'text': quote.xpath('./span[@class="text"]/text()').extract_first(),
+                'text': quote.xpath('.//span[@class="text"]/text()').extract_first(),
                 'author': quote.xpath('.//small[@class="author"]/text()').extract_first(),
                 'tags': quote.xpath('.//div[@class="tags"]/a[@class="tag"]/text()').extract()
             }
+            count = count + 1
+        print count
 
         next_page_url = response.css("li.next > a::attr(href)").extract_first()
         if next_page_url is not None:
